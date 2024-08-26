734c7fcc75131b46c18a9d1b56153d76d7d36953
Saga-sanga <vulcan248@gmail.com>
2024-08-27 00:08:40 +0530

    update spotlight path

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,12 +2,14 @@ import { Fragment } from 'react'
 import { generateMetadata } from './[slug]/page'
 import { MainHero } from '@/components/MainHero'
 import { InfoCardList } from '@/components/InfoCardList'
+import { SpotlightPreview } from '@/components/SpotlightPreview'
 
 export default async function Page() {
   return (
     <Fragment>
       <MainHero />
       <InfoCardList />
+      <SpotlightPreview />
     </Fragment>
   )
 }
