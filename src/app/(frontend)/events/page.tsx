f89343442f2a65c19859cc22713fafd27c741aed
Saga-sanga <vulcan248@gmail.com>
2024-09-09 22:04:07 +0530

    feat: add date logic for events in events page

--- src/app/(frontend)/events/page.tsx
+++ src/app/(frontend)/events/page.tsx
@@ -1,4 +1,8 @@
+import { endOfDay, getDate, getMonth, getYear, startOfDay } from 'date-fns'
 import { EventsContainer } from './components/EventsContainer'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import configPromise from '@payload-config'
+import util from 'util'
 
 // Mock data for events
 const events = [
@@ -30,7 +34,46 @@ const events = [
 
 export type Events = typeof events
 
-export default async function EventsPage() {
+export default async function EventsPage({
+  searchParams,
+}: {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}) {
+  console.log(searchParams)
+  const day = searchParams?.day ?? getDate(Date.now())
+  const month = searchParams?.month ?? getMonth(Date.now()) + 1
+  const year = searchParams?.year ?? getYear(Date.now())
+  console.log({ day, month, year })
+
+  const currentDate = new Date(`${year}-${month}-${day}`).toISOString()
+  const startTime = startOfDay(currentDate).toISOString()
+  const endTime = endOfDay(currentDate).toISOString()
+
+  // console.log({ currentDate, startTime, endTime })
+
+  const payload = await getPayloadHMR({ config: configPromise })
+  const payloadEvents = await payload.find({
+    collection: 'events',
+    depth: 1,
+    where: {
+      and: [
+        {
+          eventDate: {
+            greater_than_equal: startTime,
+          },
+        },
+        {
+          eventDate: {
+            less_than_equal: endTime,
+          },
+        },
+      ],
+    },
+  })
+
+  console.log(util.inspect(payloadEvents, { depth: null, colors: true }))
   return (
     <div className="container mx-auto py-6">
       <h1 className="text-3xl font-bold mb-6 text-primary">Calendar of Events</h1>
