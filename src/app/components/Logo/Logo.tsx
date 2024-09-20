7c8d1ac5c39ba5cd72052a27f1a4561b3fa8a562
Saga-sanga <vulcan248@gmail.com>
2024-09-20 12:36:33 +0530

    design: fine tune header logo

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      className="max-w-[9.375rem]"
+      className="max-w-[9.375rem] mr-1"
       src="/gate-logo.png"
       // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
     />
