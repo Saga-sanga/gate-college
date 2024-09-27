e0f0360127c8ed99a79729d420b374b796c8b0d3
Saga-sanga <vulcan248@gmail.com>
2024-09-27 13:59:39 +0530

    fix: build error

--- src/app/components/CallToApply.tsx
+++ src/app/components/CallToApply.tsx
@@ -6,9 +6,9 @@ export function CallToApply() {
       <div className="container flex gap-8 flex-col md:flex-row items-center justify-between">
         <div>
           <p className="basis-3/5 text-background text-2xl max-w-[38ch] tracking-wide font-serif">
-            Embark on a transformative journey at GATE Adventist Theology College, where we Guide,
-            Assist, Treat, and Educate you to become a beacon of faith and knowledge in today's
-            world.
+            {
+              "Embark on a transformative journey at GATE Adventist Theology College, where we Guide, Assist, Treat, and Educate you to become a beacon of faith and knowledge in today's world."
+            }
           </p>
           <hr className="w-[15%] border-t-4 mt-2 border-secondary" />
         </div>
