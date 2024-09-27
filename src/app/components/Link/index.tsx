45cf9cf8678d2d280fea2b39cb0a22de2e42bc74
Saga-sanga <vulcan248@gmail.com>
2024-09-27 16:20:28 +0530

    fix: type error in link

--- src/app/components/Link/index.tsx
+++ src/app/components/Link/index.tsx
@@ -42,7 +42,7 @@ export const CMSLink: React.FC<CMSLinkType> = (props) => {
 
   if (!href) return null
 
-  const size = appearance === 'link' ? 'clear' : sizeFromProps
+  const size = appearance === 'link' ? 'default' : sizeFromProps
   const newTabProps = newTab ? { rel: 'noopener noreferrer', target: '_blank' } : {}
 
   /* Ensure we don't break any styles set by richText */
