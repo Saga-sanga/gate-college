194590fdc832d9908701fff50b4f1fe0986d6377
Saga-sanga <vulcan248@gmail.com>
2024-10-24 20:49:08 +0530

    feat: add youtube lite embed from next third parties

--- src/app/components/HighlightSection/index.tsx
+++ src/app/components/HighlightSection/index.tsx
@@ -1,10 +1,11 @@
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import type { HighlightSection } from 'src/payload-types'
-import dynamic from 'next/dynamic'
-const YoutubeEmbed = dynamic(() => import('./YoutubeEmbed'), { ssr: false })
+import { YouTubeEmbed } from '@next/third-parties/google'
+// const YoutubeEmbed = dynamic(() => import('./YoutubeEmbed'), { ssr: false })
 
 export async function HighlightSection() {
   const highlight = (await getCachedGlobal('highlight-section', 1)()) as HighlightSection
+  const videoId = highlight['youtube-link'].split('=')[1]
 
   // return nothing if highlight is empty
   if (Object.keys(highlight).length === 0) {
@@ -19,7 +20,8 @@ export async function HighlightSection() {
           <hr className="w-[15%] border-t-4 mt-1 border-secondary" />
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-20">
-          <YoutubeEmbed youtubeLink={highlight['youtube-link']} title={highlight.title} />
+          <YouTubeEmbed videoid={videoId} height={315} />
+          {/* <YoutubeEmbed youtubeLink={highlight['youtube-link']} title={highlight.title} /> */}
           <div className="h-full pt-4 pe-12">
             <p className="text-3xl font-serif max-w-[38ch] tracking-wide">
               {highlight.description}
